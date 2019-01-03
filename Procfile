9674741b74ae318146356d246494e769627ba5bc
FaztTech <fazt@faztweb.com>
2019-01-03 14:22:46 -0500

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn index.py:app
---
+++ index.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Creating simple Routes 
+@app.route('/test')
+def test():
+    return "Home Page"
+
+@app.route('/test/about/')
+def about_test():
+    return "About Page"
+
+# Routes to Render Something
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about', strict_slashes=False)
+def about():
+    return render_template("about.html")
+
+# Make sure this we are executing this file
+if __name__ == '__main__':
+    app.run(debug=True)
